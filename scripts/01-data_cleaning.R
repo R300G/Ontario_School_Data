dfcf573a7e73dc64bfc784bbf1af20e3c7a6b3ec
Charles Lu <Charles10251105@gmail.com>
2022-04-29 23:40:08 -0400

    Update 01-data_cleaning.R

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,14 +1,12 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the data downloaded from https://data.ontario.ca/dataset/school-information-and-student-demographics
+# Author: Charles Lu
+# Data: 27 April 2022
+# Contact: charlesjiahong.lu@mail.utoronto.ca
 # License: MIT
 # Pre-requisites:
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+# - raw data is public and already included in the inputs/data folder
+
 
 
 #### Workspace setup ####
@@ -19,18 +17,20 @@ library(readxl)
 # Read in the raw data.
 raw_data <- read_excel("inputs/data/new_sif_data_table_2019_20prelim_en_march2022.xlsx")
 
-
+#$ clean the data
 reduced_data <-
   raw_data %>%
   filter(`School Level` == "Secondary") %>%
-  select(`School Number`, `School Type`, City, `Percentage of Students Whose First Language Is Not English`, `Percentage of Students That Passed the Grade 10 OSSLT on Their First Attempt`, `Percentage of Students Receiving Special Education Services`, `Percentage of Students Receiving Special Education Services`, `Percentage of Students Identified as Gifted`, `Percentage of School-Aged Children Who Live in Low-Income Households`, `Percentage of Students Whose Parents Have No Degree, Diploma or Certificate`) %>%
+  select(`School Type`,`Board Name`, `Percentage of Students That Passed the Grade 10 OSSLT on Their First Attempt`,`Percentage of Students Whose First Language Is Not English`, `Percentage of Students Receiving Special Education Services`, `Percentage of Students Receiving Special Education Services`, `Percentage of Students Identified as Gifted`, `Percentage of School-Aged Children Who Live in Low-Income Households`, `Percentage of Students Whose Parents Have No Degree, Diploma or Certificate`) %>%
   janitor::clean_names() %>%
   na.exclude() %>%
   filter_all(all_vars(. != "NA" & . != "SP" & . != "N/P" & . != "N/D" & . != "N/R")) %>%
   mutate(percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt = gsub("%", "", percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt)) %>%
   mutate(across(where(is.character), tolower))%>% 
-  mutate(school_type = factor(school_type))
+  mutate(school_type = factor(school_type)) %>%
+  rename(esl_students = percentage_of_students_whose_first_language_is_not_english, osslt_passes = percentage_of_students_that_passed_the_grade_10_osslt_on_their_first_attempt, se_students = percentage_of_students_receiving_special_education_services, gifted_students = percentage_of_students_identified_as_gifted, low_income_household = percentage_of_school_aged_children_who_live_in_low_income_households, no_degree_parents = percentage_of_students_whose_parents_have_no_degree_diploma_or_certificate)
 
+rm(reduced_data, raw_data)
 
-## save data_final to ‘inputs/data/cleaned_data.csv’.
-write.csv(reduced_data, "inputs/data/cleaned_data.csv", row.names = FALSE) 
+## save data_final to ‘outputs/data/cleaned_data.csv’.
+write.csv(reduced_data, "outputs/data/cleaned_data.csv", row.names = FALSE) 
